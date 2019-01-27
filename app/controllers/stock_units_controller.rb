3814119fb42474e531f955483e468e3edf74f321
Erik Sälgström Peterson <erik@barkbox.com>
2019-01-27 15:14:18 -0500

    Remove verify pagination parameters method

--- app/controllers/stock_units_controller.rb
+++ app/controllers/stock_units_controller.rb
@@ -1,4 +1,4 @@
-class StockUnitsController < ActionController::API
+class StockUnitsController < ApplicationController
   before_action :authenticate_user!
 
   def index
@@ -66,24 +66,5 @@ class StockUnitsController < ActionController::API
       :stock_unit_type_id,
       :unit_attributes => {}
     )
-  end
-
-  def validate_pagination_params!
-    sort = params[:sort] || 'desc'
-    if !['asc', 'desc'].include?(sort)
-      raise "#{sort} is not a valid value for `sort'"
-    end
-
-    page = params[:page] ? params[:page].to_i : 1
-    if page < 1
-      raise "#{page} is not a valid value for `page'"
-    end
-
-    per_page = params[:per_page] ? params[:per_page].to_i : 10
-    if per_page < 1
-      raise "#{per_page} is not a valid value for `page'"
-    end
-
-    [sort, page, per_page]
   end
 end
